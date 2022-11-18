40f8c8f6228ba91c18ea8129a66dba9635b75b82
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-11-18 22:15:02 +0300

    add creating todoLists and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 import './App.css';
 import {v1} from 'uuid';
 import { Todolist } from './TodoList/Todolist';
+import {Input} from "./common/Input/Input";
 
+type TodoListType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
 export type TaskType = {
     id: string
     title: string
@@ -11,6 +17,7 @@ export type TaskType = {
 type TasksType = {
     [key: string]: TaskType[]
 }
+export type FilterValuesType = 'all' | 'completed' | 'active'
 
 function App() {
     let todolistId1 = v1();
@@ -32,20 +39,30 @@ function App() {
         ]
     });
 
+    const addNewTodoList = (todoTitle: string) => {
+        const newTodoID = v1()
+        setTodolists([...todolists, {id: newTodoID, title: todoTitle, filter: "all"}])
+        setTasks({...tasks, [newTodoID]: []})
+    }
+    const addNewTask = (todoListID: string) => (taskTitle: string) => {
+        setTasks({...tasks, [todoListID]:[{id: v1(), title: taskTitle, isDone: false}, ...tasks[todoListID]]})
+    }
+
 
 
     return (
         <div className="App">
             <div style={{height: '30px', marginTop: '20px'}}>
-                <input type="text"/>
-                <button>+</button>
+                <Input placeholder={'Enter new title'} callback={addNewTodoList}/>
             </div>
             {todolists.map(tl=>{
 
 
                 return  <Todolist
+                    todoListID={tl.id}
                     todoTitle={tl.title}
                     tasks={tasks[tl.id]}
+                    addNewTask={addNewTask}
                 />
             })}
 
