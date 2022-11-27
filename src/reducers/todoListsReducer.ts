6efd76c587aa8e2af7027df113c6031f10c3b320
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-11-27 12:20:11 +0300

    add filtering tasks

--- src/reducers/todoListsReducer.ts
+++ src/reducers/todoListsReducer.ts
@@ -1,4 +1,4 @@
-import {SortValuesType, TodoListType} from "../App";
+import {FilterValuesType, SortValuesType, TodoListType} from "../App";
 
 
 export const todoListsReducer = (state: TodoListType[], action: ActionsType): TodoListType[] => {
@@ -13,16 +13,21 @@ export const todoListsReducer = (state: TodoListType[], action: ActionsType): To
                 }])
         case "CHANGE_SORT_VALUE":
             return state.map(tl => tl.id === action.payload.todoListID ? {...tl, sort: action.payload.sort} : tl)
+        case "CHANGE_FILTER_VALUE":
+            return state.map(tl=>tl.id===action.payload.todoListID?{...tl,filter:action.payload.filterValue}:tl)
         default:
             throw new Error('Action not supported')
     }
 }
 
-type ActionsType = AddNewTodoListType | ChangeSortValueType
+type ActionsType = AddNewTodoListType | ChangeSortValueType | ChangeFilterValueType
 type AddNewTodoListType = ReturnType<typeof addNewTodoListAC>
 type ChangeSortValueType = ReturnType<typeof changeSortValueAC>
+type ChangeFilterValueType = ReturnType<typeof changeFilterValueAC>
 
 export const addNewTodoListAC = (todoTitle: string, newTodoID: string) => (
     {type: "ADD_NEW_TODO_LIST", payload: {todoTitle, newTodoID}} as const)
 export const changeSortValueAC = (todoListID: string, sort: SortValuesType) => (
     {type: "CHANGE_SORT_VALUE", payload: {todoListID, sort}} as const)
+export const changeFilterValueAC = (todoListID: string, filterValue: FilterValuesType) => (
+    {type: "CHANGE_FILTER_VALUE", payload: {todoListID, filterValue}} as const)
