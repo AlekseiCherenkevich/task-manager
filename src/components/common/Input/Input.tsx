c3d1c9b94509228bf9db8e8b3335c291d794cd7b
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-12-06 21:17:39 +0300

    fix universal input

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,24 +1,25 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
 type InputPropsType = {
-    onKeyPress: () => void
+    value: string
+    onKeyPress: (value: string) => void
+    onChange: (value: string) => void
 }
 
 const Input: React.FC<InputPropsType> = (props) => {
-    const [value, setValue] = useState('')
     const [error, setError] = useState('')
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         if (error) {
             setError('')
         }
-        setValue(e.currentTarget.value)
+        props.onChange(e.currentTarget.value)
     }
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-            if (value.trim().length) {
-                props.onKeyPress()
-                setValue('')
+            if (props.value.trim().length) {
+                props.onKeyPress(e.currentTarget.value)
+                props.onChange('')
                 setError('')
             } else {
                 setError('Field is required')
@@ -32,7 +33,7 @@ const Input: React.FC<InputPropsType> = (props) => {
     }
 
     return <div>
-        <input value={value}
+        <input value={props.value}
                onChange={onChangeHandler}
                onKeyPress={onKeyPressHandler}
                onBlur={onBlurHandler}
