b50ee9fd318189e4eaccb587e7f9f843ea6a8ad4
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-12-25 14:13:06 +0300

    add validation to input

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,20 +1,59 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 type AddItemFormType = {
     onClick: (value: string) => void
+    placeholder?: string
 }
 
 export const AddItemForm: React.FC<AddItemFormType> = (props) => {
     const [value, setValue] = useState('')
+    const [error, setError] = useState('')
 
-    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+    useEffect(()=>{
+        if (value.trim().length!==0) {
+            setError('')
+        }
+    },[value])
+
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
     const onCLickHandler = () => {
-        props.onClick(value)
-        setValue('')
+        if (value.trim().length!==0) {
+            props.onClick(value)
+            setValue('')
+        } else {
+            setError('Field is required')
+            setValue('')
+        }
+    }
+    const onEnterPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            onCLickHandler()
+        }
+    }
+    const onBlurHandler = () => {
+        setError('')
     }
 
-    return <div>
-        <input value={value} onChange={onChangeHandler} type="text"/>
-        <button onClick={onCLickHandler}>+</button>
+
+    return <div style={{height: '70px'}}>
+        <TextField value={value}
+                   onChange={onChangeHandler}
+                   onBlur={onBlurHandler}
+                   onKeyPress={onEnterPressHandler}
+                   error={!!error}
+                   label={props.placeholder}
+                   helperText={error}
+                   size={"small"}
+                   />
+        <Button size={"small"}
+                variant={"contained"}
+                style={{minHeight: '40px', maxHeight: '40px', minWidth: '40px', maxWidth: '40px'}}
+            onClick={onCLickHandler}
+        >+</Button>
     </div>
 }
+
