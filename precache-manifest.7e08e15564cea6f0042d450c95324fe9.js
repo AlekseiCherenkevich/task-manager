632281d643d12029e0bd4124cb43f2de46dfa690
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-04-14 00:39:49 +0300

    Updates

---
+++ precache-manifest.7e08e15564cea6f0042d450c95324fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773fba9bfc625163e539ea3069e97cf3",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "821ba6ae6fb5848dc476",
+    "url": "/task-manager/static/css/main.c9c3dcf0.chunk.css"
+  },
+  {
+    "revision": "bc26d9a8397f47b9050e",
+    "url": "/task-manager/static/js/2.4ac33e01.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/task-manager/static/js/2.4ac33e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821ba6ae6fb5848dc476",
+    "url": "/task-manager/static/js/main.1f3cf1bd.chunk.js"
+  },
+  {
+    "revision": "bc500f03bcb32101dfe5",
+    "url": "/task-manager/static/js/runtime-main.f41001b5.js"
+  }
+]);
